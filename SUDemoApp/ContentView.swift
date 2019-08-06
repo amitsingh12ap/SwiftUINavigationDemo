a98ecb2dd8acace55b4182e3ce9f0b59eeea492f
Amit <amit.singh@yatra.com>
2019-08-06 11:07:02 +0530

    Initial Commit

---
+++ SUDemoApp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SUDemoApp
+//
+//  Created by Amit Singh on 06/08/19.
+//  Copyright © 2019 Amit Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
